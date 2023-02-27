6c232f60db0d1f46d32e7367624c1b1d364a52cb
Dita Mukane <dita.mukane@gmail.com>
2023-02-27 11:43:00 +0100

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { DetailsComponent } from './details/details.component';
+import { BioComponent } from './bio/bio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+    PortfolioComponent,
+    DetailsComponent,
+    BioComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
