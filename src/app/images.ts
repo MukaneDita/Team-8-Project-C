4f669a612310929b1b559d88e603e49e233c4901
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-02-27 16:28:32 +0100

    add array with images(part)

---
+++ src/app/images.ts
@@ -0,0 +1,24 @@
+let images = [{
+    link1: "./assets/img/concerts(horizontal)/concert.jpg",
+    link2: "./assets/img/concerts(horizontal)/concert1.jpg",
+    link3: "./assets/img/concerts(horizontal)/concert2.jpg",
+    link4: "./assets/img/concerts(horizontal)/concert3.jpg",
+    link5: "./assets/img/concerts(horizontal)/concert4.jpg",
+    link6: "./assets/img/concerts(horizontal)/concert5.jpg",
+    link7: "./assets/img/concerts(horizontal)/concert6.jpg",
+    link8: "./assets/img/concerts(horizontal)/concert7.jpg",
+    link9: "./assets/img/concerts(horizontal)/concert8.jpg",
+    link10: "./assets/img/concerts(horizontal)/concert9.jpg",
+}, {
+    link1: "./assets/img/portraits(vertical)/portrait.jpg",
+    link2: "./assets/img/portraits(vertical)/portrait1.jpg",
+    link3: "./assets/img/portraits(vertical)/portrait2.jpg",
+    link4: "./assets/img/portraits(vertical)/portrait3.jpg",
+    link5: "./assets/img/portraits(vertical)/portrait4.jpg",
+    link6: "./assets/img/portraits(vertical)/portrait5.jpg",
+    link7: "./assets/img/portraits(vertical)/portrait6.jpg",
+    link8: "./assets/img/portraits(vertical)/portrait7.jpg",
+    link9: "./assets/img/portraits(vertical)/portrait8.jpg",
+    link10: "./assets/img/portraits(vertical)/portrait9.jpg",
+}]
+
