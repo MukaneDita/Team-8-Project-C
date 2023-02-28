9b51bbc8b8fadce0ab36805eca5482878defac0f
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-02-28 11:29:40 +0100

    add cart and changes to shop array

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { shop, Ishop } from '../shop';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  // cart: Ishop[] = [];
+  // total: number =0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    // this.cart = this.cartService.getItems();
+
+  }
 
 }
