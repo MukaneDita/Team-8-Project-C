d59914aff9500d196ae3b53670748c8a4cd03a00
Dita Mukane <dita.mukane@gmail.com>
2023-02-28 15:18:46 +0100

    bio

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -12,17 +12,18 @@ export class BioComponent {
   info = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
-    email:new FormControl(''),
+    email: new FormControl(''),
     message: new FormControl(''),
+    subject: new FormControl('')
   });
 
-  onSubmit(){
+  onSubmit() {
     // to take the values from the form you can select the property that have the formGroup and then just add .value
-   
-      var a = this.info.value;
-      console.log(a)
-   
-    }
-  
+
+    var a = this.info.value;
+    console.log(a)
+
+  }
+
 
 }
