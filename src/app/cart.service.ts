077abd6cfedcedc6a927683dab0aeffa14990b1f
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-28 11:33:43 +0100

    added the cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Ishop } from './shop';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+cart : Array<Ishop> = [];
   constructor() { }
+
+  addToCart(photo: Ishop) {
+    this.cart.push(photo);
+  }
+
+  getCart() {
+    return this.cart;
+  }
+
+  clearCart() {
+    this.cart = [];
+    return this.cart;
+  }
+  total() {
+    let total: number = 0;
+    for(let val of this.cart) {
+      total = total + (val.price * val.qtty);
+    }
+    return total;
+  }
 }
