94c235645491be988b405af813d9a4a668d5c053
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-02-28 11:54:50 +0100

    add app routing and shop

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { shop, Ishop } from '../shop'; 
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
-export class ShopComponent {
+export class ShopComponent  implements OnInit {
+  shop: Array<Ishop> = shop;
+  cart: Ishop[] = [];
 
+  constructor(private cartService: CartService) { }
+
+  addToCart(val: Ishop) {
+    window.alert('Your order has been added to the cart!');
+    this.cartService.addToCart(val);
+  }
+
+  ngOnInit(): void {
+  }
 }
